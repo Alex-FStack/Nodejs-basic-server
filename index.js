28b47c1f4d172ab3aeeada73385900e9807c7388
Alex <alxpop56@gmail.com>
2022-03-30 12:08:26 +1100

    express basics

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
+const path = require('path')
 
-const homePage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notfoundPage = fs.readFileSync('notfound.html')
+const app = express() //call to express function to start new express app
+app.use(express.static('public'))
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/about')
-        res.end(aboutPage)
-    else if(req.url === '/contact')
-        res.end(contactPage)
-    else if(req.url === '/')
-        res.end(homePage)
-    else{
-        res.writeHead(404)
-        res.end(notfoundPage)
-    }
-
-    console.log(req.url)
-    res.end('Hello Node.js')
+app.listen(3000,() => {
+    console.log("app listening on port 3000")
 })
 
-server.listen(3000)
+app.get('/', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.get('/about', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, 'about.html'))
+})
+
+app.get('/contact', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, 'contact.html'))
+})
+
+
+
+// const homePage = fs.readFileSync('index.html')
+// const aboutPage = fs.readFileSync('about.html')
+// const contactPage = fs.readFileSync('contact.html')
+// const notfoundPage = fs.readFileSync('notfound.html')
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === '/about')
+//         res.end(aboutPage)
+//     else if(req.url === '/contact')
+//         res.end(contactPage)
+//     else if(req.url === '/')
+//         res.end(homePage)
+//     else{
+//         res.writeHead(404)
+//         res.end(notfoundPage)
+//     }
+
+//     console.log(req.url)
+//     res.end('Hello Node.js')
+// })
+
+// server.listen(3000)
